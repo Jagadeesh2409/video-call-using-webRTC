956fa96dfc3331bd4089cef9e96dabfc05a29e1f
Jagadeesh2409 <jagadeesh242004@gmail.com>
2025-12-02 17:03:43 +0530

    Implement WebRTC video call functionality with UI and socket communication

--- socket/index.js
+++ socket/index.js
@@ -1,6 +1,7 @@
 module.exports = (io) => {
   io.on("connection", (socket) => {
     console.log("a user connected:", socket.id);
+    require('./live')(io,socket)
 
     socket.on('disconnect', () => {
       console.log('user disconnected:', socket.id);
